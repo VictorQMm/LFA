f5fa736ee83a3cf134b27300df51db5c49f7bb58
Victor Querino Martins <123746753+VictorQMm@users.noreply.github.com>
2024-05-19 17:37:29 -0300

    att

--- Trabalhos/Trabalho.c
+++ Trabalhos/Trabalho.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int main(){
+int main() {
     int estadoI;
     int qtdEstado;
     int qtdEstadoF;
@@ -10,9 +10,8 @@ int main(){
     int estadoA;
     char palavra[20];
 
-// Pedindo para o usuário inserie as entradas do programa.
-
-    printf("Estado Inicial: ");     
+    // Pedindo para o usuário inserir as entradas.
+    printf("Estado Inicial: ");
     scanf("%d", &estadoI);
 
     printf("Quantidade de Estados: ");
@@ -21,26 +20,23 @@ int main(){
     printf("Quantidade de Estados Finais: ");
     scanf("%d", &qtdEstadoF);
 
-
-for(int i = 0; i < qtdEstadoF; i++) {                   // For usado para repetir quantas vezes  de estados finais o usuário inserir
-        printf("Digite o %d estado final: \n", i + 1);
+    for (int i = 0; i < qtdEstadoF; i++) {  // For usado para repetir quantas vezes de estados finais o usuário inserir
+        printf("Digite o %d estado final: ", i + 1);
         scanf("%d", &estadoF[i]);
     }
 
-    printf("Quantidade simbolos no alfabrto: ");
+    printf("Quantidade de simbolos no alfabeto: ");
     scanf("%d", &qtdAlfa);
 
-for(int i = 0; i < qtdAlfa; i++) {                      // For usado para repetir quantos simbolos o usuário inserir
-        printf("Digite o %d simbolo: \n", i + 1);
+    for (int i = 0; i < qtdAlfa; i++) {  // For usado para repetir quantos simbolos o usuário inserir
+        printf("Digite o %d simbolo: ", i + 1);
         scanf(" %c", &alfa[i]);
     }
 
-   
-int tabela [qtdEstado][qtdAlfa];    // Criando matriz para a tabela
+    int tabela[qtdEstado][qtdAlfa];  // Criando matriz para a tabela
 
-    printf("Preenchja a Tabela de Transicao: \n");
-  
-for (int i = 0; i < qtdEstado; i++) {           //For usado para preencher a matriz
+    printf("Preencha a Tabela de Transicao: \n");
+    for (int i = 0; i < qtdEstado; i++) {  // For usado para preencher a matriz
         for (int j = 0; j < qtdAlfa; j++) {
             printf("(Q%d, %c): ", i + 1, alfa[j]);
             scanf("%d", &tabela[i][j]);
@@ -49,52 +45,58 @@ for (int i = 0; i < qtdEstado; i++) {           //For usado para preencher a mat
 
     // Printar a tabela
     printf("Estado   ");
-for (int i = 0; i < qtdAlfa; i++) {
+    for (int i = 0; i < qtdAlfa; i++) {
         printf(" %c ", alfa[i]);
     }
     printf("\n");
 
-for(int i = 0; i < qtdEstado; i++){
-    printf("K%d        ", i + 1 );
-        for(int j = 0; j < qtdAlfa; j++){
-            printf("%d  ",tabela[i][j]);
+    for (int i = 0; i < qtdEstado; i++) {
+        printf("Q%d      ", i + 1);
+        for (int j = 0; j < qtdAlfa; j++) {
+            printf("%d  ", tabela[i][j]);
         }
         printf("\n");
     }
 
-
     // Verificação de palavras
-
     printf("Digite a palavra a ser verificada: ");
-        scanf("%s", palavra);
+    scanf("%s", palavra);
 
     estadoA = estadoI;
 
-    // Verifico se o caractere pertence ao alfabeto
-
-    for (int i = 0; palavra[i] != '\0'; i++) {     
-        int indice = -1;                            //  -1 indice é uma maneira de falar não foi encontrada. Isso mostra q o caractere não está no alfabeto.
+    // Verificar se o caractere pertence ao alfabeto
+    for (int i = 0; palavra[i] != '\0'; i++) {
+        int indice = -1;  // -1 indica que o caractere não foi encontrado no alfabeto.
         for (int j = 0; j < qtdAlfa; j++) {
             if (palavra[i] == alfa[j]) {
-                indice = j;                     // para salvar o indice (caractere), em J   
+                indice = j;  // para salvar o índice (caractere), em j
                 break;
             }
         }
 
-        //Se indice ainda é -1, não esta presente no alfa.
-        
-        estadoA = tabela[estadoA - 1][indice];      //Atualiza o estado
+        // Se índice ainda é -1, não está presente no alfabeto.
+        if (indice == -1) {
+            printf("Palavra contem simbolo nao pertencente ao alfabeto.\n");
+            return 1;
+        }
+
+        estadoA = tabela[estadoA - 1][indice];  // Atualiza o estado
     }
 
-// Faz a verificação de estado
-
+    // Verifica o estado, para ver se é um estado aceitável
+    int aceita = 0;
     for (int i = 0; i < qtdEstadoF; i++) {
         if (estadoA == estadoF[i]) {
-            printf("Palavra aceita !!!!.\n");
-        }else{
-
-            printf("Palavra nao aceita ;-;.\n");
+            aceita = 1;
+            break;
         }
     }
 
+    if (aceita) {
+        printf("Palavra aceita !!!!.\n");
+    } else {
+        printf("Palavra nao aceita ;-;.\n");
+    }
+
+    return 0;
 }
